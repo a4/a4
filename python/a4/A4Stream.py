685194054f42247e75d070a1e77b74056e299b2a
Johannes Ebke <johannes.ebke@physik.uni-muenchen.de>
2011-06-19 02:25:55 +0200

    update for binary format, enable concatenation\!

--- python/a4/A4Stream.py
+++ python/a4/A4Stream.py
@@ -1,4 +1,3 @@
-from io import IOBase
 from struct import pack, unpack
 
 from messages.A4Stream_pb2 import A4StreamHeader, A4StreamFooter, A4StartCompressedSection, A4EndCompressedSection
@@ -11,40 +10,49 @@ END_MAGIC = "KTHXBYE4"
 HIGH_BIT = (1<<31)
 SEEK_END = 2
 
-class A4WriterStream(IOBase):
-    def __init__(self, out_stream, content=None):
+class A4WriterStream(object):
+    def __init__(self, out_stream, content=None, content_cls=None):
         self.bytes_written = 0
         self.content_count = 0
         self.previous_type = 0
+        self.content_type = None
         self.out_stream = out_stream
+        self.write_header(content, content_cls)
+
+    def write_header(self, content_name=None, content_cls=None):
         self.out_stream.write(START_MAGIC)
         self.bytes_written += len(START_MAGIC)
         header = A4StreamHeader()
         header.a4_version = 1
-        if content:
-            header.content = content
+        if content_name:
+            header.content = content_name
+            self.content_type = content_cls.CLASS_ID_FIELD_NUMBER
+        else:
+            self.content_type = None
         self.write(header)
 
-    def close(self):
+    def write_footer(self):
         footer = A4StreamFooter()
         footer.size = self.bytes_written
-        footer.content_count = self.content_count
+        if self.content_type:
+            footer.content_count = self.content_count
         self.write(footer)
         self.out_stream.write(pack("!I", footer.ByteSize()))
         self.out_stream.write(END_MAGIC)
         self.bytes_written += len(END_MAGIC)
+
+    def close(self):
+        self.write_footer()
         return self.out_stream.close()
 
-    @property
-    def closed(self):
-        return self.out_stream.closed
-    
     def flush(self):
         return self.out_stream.flush()
     
     def write(self, o):
         type = o.CLASS_ID_FIELD_NUMBER
         size = o.ByteSize()
+        if type == self.content_type:
+            self.content_count += 1
         assert 0 <= size < HIGH_BIT, "Message size not in range!"
         assert 0 < type < HIGH_BIT, "Type ID not in range!"
         if type != self.previous_type:
@@ -58,46 +66,67 @@ class A4WriterStream(IOBase):
         self.out_stream.write(o.SerializeToString())
         self.bytes_written += size
 
-class A4ReaderStream(IOBase):
+
+class A4ReaderStream(object):
     def __init__(self, in_stream):
+        self.numbering = dict(numbering)
         self.previous_type = None
         self.in_stream = in_stream
         self.size = 0
-        self.headers = []
+        self.headers = {}
+        self.footers = {}
+        self.read_header()
+        while True:
+            self.read_footer(self.size)
+            self.in_stream.seek(-self.size, SEEK_END)
+            self.read_header()
+            first = False
+            self.in_stream.seek(-self.size, SEEK_END)
+            if self.in_stream.tell() == 0:
+                break
+        self.in_stream.seek(len(START_MAGIC))
+
+    def read_header(self):
         # get the beginning-of-stream information
+        header_position = self.in_stream.tell()
         assert START_MAGIC == self.in_stream.read(len(START_MAGIC)), "Not an A4 file!"
         cls, header = self.read_message()
         assert header.a4_version == 1, "Incompatible stream version :( Upgrade your client?"
-        self.header = header
-        # get the end-of-stream information
-        self.in_stream.seek(-len(END_MAGIC), SEEK_END)
+        self.headers[header_position] = header
+
+    def read_footer(self, neg_offset=0):
+        self.in_stream.seek(-neg_offset - len(END_MAGIC), SEEK_END)
         if not END_MAGIC == self.in_stream.read(len(END_MAGIC)):
-            print("File seems to be not closed! Continuing anyway...")
+            print("File seems to be not closed!")
             self.in_stream.seek(0)
-            return     
-        self.in_stream.seek(-len(END_MAGIC)-4, SEEK_END)
-        footer_size,  = unpack("!I", self.in_stream.read(4))
-        footer_start = -len(END_MAGIC) - 4 - footer_size - 8
+            return None
+        self.in_stream.seek(-neg_offset - len(END_MAGIC) - 4, SEEK_END)
+        footer_size, = unpack("!I", self.in_stream.read(4))
+        footer_start = - neg_offset - len(END_MAGIC) - 4 - footer_size - 8
         self.in_stream.seek(footer_start, SEEK_END)
         cls, footer = self.read_message()
-        self.footer = footer
-        self.size = footer.size - footer_start
-        # Try to find if this is a concatenated file
-        self.in_stream.seek(-self.size, SEEK_END)
-        if not self.in_stream.tell() == 0:
-            print("WARNING: concatenating files not supported yet")
-        self.in_stream.seek(len(START_MAGIC))
+        self.footers[footer_start] = footer
+        self.size += footer.size - footer_start - neg_offset
+
+    def register(self, cls):
+        self.numbering[cls.CLASS_ID_FIELD_NUMBER] = cls
 
     def info(self):
         info = []
-        info.append("A4 file v%i" % self.header.a4_version)
+        version = [h.a4_version for h in self.headers.values()]
+        info.append("A4 file v%i" % version[0])
         info.append("size: %s bytes" % self.size)
-        if self.header.content:
-            if self.footer:
-                c = self.footer.content_count
-                info.append("content: %i %s%s" % (c, self.header.content, "s" if c != 1 else ""))
+        hf = zip(self.headers.values(), self.footers.values())
+        cccd = {}
+        for c, cc in ((h.content, f.content_count) for h, f in hf):
+            cccd[c] = cccd.get(c, 0) + cc
+        for content in sorted(cccd.keys()):
+            cc = cccd[content]
+            if cc != 1:
+                ms = "s"
             else:
-                info.append("content: %s" % (self.header.content))
+                ms = ""
+            info.append("%i %s%s" % (cccd[content], content, ms))
         return ", ".join(info)
 
     def read_message(self):
@@ -108,14 +137,12 @@ class A4ReaderStream(IOBase):
             self.previous_type = type
         else:
             type = self.previous_type
-        cls = numbering[type]
+        cls = self.numbering[type]
         return cls, cls.FromString(self.in_stream.read(size))
 
     def read(self):
         cls, message = self.read_message()
         if cls is A4StreamHeader:
-            self.headers.append(message)
-            self.header = message
             return self.read()
         if cls is A4StreamFooter:
             footer_size,  = unpack("!I", self.in_stream.read(4))
--- tests/test_simple.py
+++ tests/test_simple.py
@@ -2,23 +2,38 @@
 
 def test_writing():
     from a4 import A4WriterStream
-    w = A4WriterStream(open("test.a4", "w"), content="Event")
-    from a4.A4Stream import A4StartCompressedSection
-    from a4.A4Stream import A4EndCompressedSection
+    from a4.A4Stream import A4StartCompressedSection, A4EndCompressedSection
+    from a4.messages.Event_pb2 import Event
+    w = A4WriterStream(open("test.a4", "w"), content="Event", content_cls = Event)
     s =A4StartCompressedSection()
     s.compression = s.BZIP2
     w.write(s)
     s = A4EndCompressedSection()
     s.size = 42
     w.write(s)
+    e = Event()
+    e.event_number = 1000
+    e.mc_event_weight = 1.2
+    w.write(e)
+    e.event_number = 1001
+    e.mc_event_weight = 1.1
+    w.write(e)
+    e.event_number = 1002
+    e.mc_event_weight = 0.9
+    w.write(e)
     w.close()
 
 def test_reading():
     from a4 import A4ReaderStream
+    from a4.messages.Event_pb2 import Event
     r = A4ReaderStream(open("test.a4"))
+    r.register(Event)
     msg1 = r.read()
     assert msg1.compression == msg1.BZIP2
     msg2 = r.read()
     assert msg2.size == 42
+    assert r.read().event_number == 1000
+    assert r.read().event_number == 1001
+    assert r.read().event_number == 1002
     assert r.read() is None
     assert r.info()
