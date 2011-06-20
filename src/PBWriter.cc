9df80f5f9b560b2d50c66635c010186d2a5f190c
Johannes Ebke <johannes.ebke@physik.uni-muenchen.de>
2011-06-20 18:24:08 +0200

    wrote C++ writer

--- src/PBWriter.cc
+++ src/PBWriter.cc
@@ -7,6 +7,9 @@
 
 #include <string>
 
+#include <boost/shared_ptr.hpp>
+
+#include <google/protobuf/descriptor.h>
 #include <google/protobuf/stubs/common.h>
 #include <google/protobuf/io/coded_stream.h>
 #include <google/protobuf/io/gzip_stream.h>
@@ -17,51 +20,75 @@
 
 using namespace std;
 
-using pb::Writer;
+const string START_MAGIC = "A4STREAM";
+const string END_MAGIC = "KTHXBYE4";
+const uint32_t HIGH_BIT = (1<<31);
 
-Writer::Writer(const fs::path &output_file):
+Writer::Writer(const fs::path &output_file, string content_name = "", uint32_t content_type = 0):
     _output(output_file.string().c_str(),
             ios::out | ios::trunc | ios::binary),
-    _events_written(0)
+    _content_count(0),
+    _bytes_written(0),
+    _content_type(content_type)
 {
-
     _raw_out.reset(new ::google::protobuf::io::OstreamOutputStream(&_output));
-    _raw_compressed_out.reset(new ::google::protobuf::io::GzipOutputStream(_raw_out.get()));
-    _coded_out.reset(new ::google::protobuf::io::CodedOutputStream(_raw_compressed_out.get()));
 
-    _coded_out->WriteLittleEndian32(_events_written);
+    _coded_out.reset(new ::google::protobuf::io::CodedOutputStream(_raw_out.get()));
+
+    write_header(content_name);
 }
 
 Writer::~Writer()
 {
+    write_footer();
     _coded_out.reset();
     _raw_out.reset();
-    _raw_compressed_out.reset();
-
-    _output.seekp(0);
-
-    _raw_out.reset(new ::google::protobuf::io::OstreamOutputStream(&_output));
-    _raw_compressed_out.reset(new ::google::protobuf::io::GzipOutputStream(_raw_out.get()));
-    _coded_out.reset(new ::google::protobuf::io::CodedOutputStream(_raw_compressed_out.get()));
-    _coded_out->WriteLittleEndian32(_events_written);
-
-    _coded_out.reset();
-    _raw_out.reset();
-    _raw_compressed_out.reset();
-
     _output.close();
 }
 
-bool Writer::write(const Event &event)
+bool Writer::write_header(string content_name) {
+    _coded_out->WriteString(START_MAGIC);
+    _bytes_written += START_MAGIC.size();
+
+    A4StreamHeader header;
+    header.set_a4_version(1);
+    if (_content_type != 0)
+        header.set_content(content_name);
+    return write(MessagePtr(&header));
+}
+
+bool Writer::write_footer() {
+    A4StreamFooter footer;
+    footer.set_size(_bytes_written);
+    if (_content_type != 0)
+        footer.set_content_count(_content_count);
+    write(MessagePtr(&footer));
+    _coded_out->WriteLittleEndian32(footer.ByteSize());
+    _coded_out->WriteString(END_MAGIC);
+    _bytes_written += END_MAGIC.size();
+    return true;
+}
+
+bool Writer::write(Writer::MessagePtr msg)
 {
     string message;
-    if (!event.SerializeToString(&message))
+    if (!msg->SerializeToString(&message))
         return false;
 
-    _coded_out->WriteVarint32(message.size());
+    uint32_t type = msg->GetDescriptor()->FindFieldByName("CLASS_ID")->number();
+    uint32_t size = message.size();
+    if (type == _content_type) _content_count++;
+    if (type != _previous_type) {
+        _coded_out->WriteLittleEndian32(size | HIGH_BIT );
+        _coded_out->WriteLittleEndian32(type);
+        _bytes_written += 8;
+        _previous_type = type;
+    } else {
+        _coded_out->WriteLittleEndian32(size);
+        _bytes_written += 4;
+    }
+
     _coded_out->WriteString(message);
-
-    ++_events_written;
-
+    _bytes_written += size;
     return true;
 }
--- src/PBWriter.h
+++ src/PBWriter.h
@@ -9,38 +9,48 @@
 #define PROTOBUF_WRITER_H
 
 #include <fstream>
+#include <string>
 
 #include <boost/filesystem.hpp>
 #include <boost/shared_ptr.hpp>
 
-#include "pb/Event.pb.h"
+#include "pb/A4Stream.pb.h"
+
 
 namespace fs = boost::filesystem;
+using std::string;
 
-namespace pb
+class Writer
 {
-    class Writer
-    {
-        public:
-            Writer(const fs::path &output_file);
-            ~Writer();
+    public:
+        Writer(const fs::path &output_file, string content_name, uint32_t content_cls);
+        ~Writer();
 
-            bool write(const Event &);
+        typedef boost::shared_ptr< ::google::protobuf::Message> MessagePtr;
+        bool write(MessagePtr m);
 
-        private:
-            std::fstream _output;
+    private:
 
-            boost::shared_ptr< ::google::protobuf::io::ZeroCopyOutputStream>
-                _raw_out;
 
-            boost::shared_ptr< ::google::protobuf::io::ZeroCopyOutputStream>
-                _raw_compressed_out;
+        std::fstream _output;
 
-            boost::shared_ptr< ::google::protobuf::io::CodedOutputStream>
-                _coded_out;
+        boost::shared_ptr< ::google::protobuf::io::ZeroCopyOutputStream>
+            _raw_out;
 
-            uint32_t _events_written;
-    };
-}
+        boost::shared_ptr< ::google::protobuf::io::ZeroCopyOutputStream>
+            _raw_compressed_out;
+
+        boost::shared_ptr< ::google::protobuf::io::CodedOutputStream>
+            _coded_out;
+
+        bool write_header(string content_name);
+        bool write_footer();
+
+        uint32_t _content_count;
+        uint32_t _bytes_written;
+        uint32_t _content_type;
+        uint32_t _previous_type;
+
+};
 
 #endif
