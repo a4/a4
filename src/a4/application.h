3b0b8020d4a1d8c9bfd01a7e288d0e46416c18b0
Johannes Ebke <johannes.ebke@physik.uni-muenchen.de>
2011-06-21 12:15:29 +0200

    added A4_MAIN macro for simple processors

--- src/a4/application.h
+++ src/a4/application.h
@@ -2,6 +2,10 @@
 #define A4_APPLICATION_H
 
 #include "a4/processor.h"
+
+#define A4_MAIN(PROCESSOR) class MyProcessingJob : public ProcessingJob { public: virtual ProcessorPtr get_processor() { return ProcessorPtr(new PROCESSOR()); }; };\
+                           int main(int argc, char ** argv) { ProcessingJobPtr pf(new MyProcessingJob()); return a4_main(argc, argv, pf); };
+
 int a4_main(int argc, char *argv[], ProcessingJobPtr pf);
 
 #endif
