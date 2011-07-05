8ceb1641e45aa435dfb0f5f0fa18c58ca7e22503
Johannes Ebke <johannes.ebke@physik.uni-muenchen.de>
2011-07-05 14:37:09 +0200

    added default processing

--- python/processing.py
+++ python/processing.py
@@ -38,13 +38,13 @@ def get_a4_cmd(cmd, in_dir, res_out_d, skim_out_d):
     cmdl.extend(fls)
     return " ".join(cmdl)
 
-
-def process(cmd, in_dirs, res_out_d=None, skim_out_d=None, dry_run=False):
+def get_a4_cmds(cmd, in_dirs, res_out_d=None, skim_out_d=None):
     assert res_out_d or skim_out_d
     from os import getenv, getcwd
     opts = "cd %s; LD_LIBRARY_PATH=%s PATH=%s" % (getcwd(), getenv("LD_LIBRARY_PATH"), getenv("PATH"))
-    cmds = [get_a4_cmd(" ".join((opts,cmd)), d, res_out_d, skim_out_d) for d in in_dirs]
+    return [get_a4_cmd(" ".join((opts,cmd)), d, res_out_d, skim_out_d) for d in in_dirs]
 
+def process(cmds, dry_run=False):
     if dry_run:
         for cmd in cmds[::-1]:
             print cmd
@@ -60,12 +60,33 @@ def process(cmd, in_dirs, res_out_d=None, skim_out_d=None, dry_run=False):
 if __name__=="__main__":
     from sys import argv
     from optparse import OptionParser
+    from glob import glob
     parser = OptionParser()
     parser.add_option("-r", "--results", dest="results", default=None, help="result directory", metavar="DIR")
     parser.add_option("-s", "--skim", dest="skims", default=None, help="skim directory", metavar="DIR")
-    parser.add_option("-n", "--nothing", dest="dryrun", default=False, help="do a dry run", metavar="DIR")
+    parser.add_option("-n", "--nothing", dest="dryrun", action="store_true", default=False, help="do a dry run")
+    parser.add_option("-d", "--defaultdir", dest="defaultdir", default=None, help="specify a dir with /mc /egamma and /muons subdirectories, which will be processed by cmd_mc, cmd_egamma and cmd_muons", metavar="DIR")
     (options, args) = parser.parse_args()
     cmd = args[0]
     inputs = args[1:]
-    process(cmd, inputs, options.results, options.skims, options.dryrun)
+    print options.results
+    if options.defaultdir:
+        assert len(inputs) == 0
+        input = options.defaultdir
+        cmds = []
+        assert isdir(pjoin(input, "mc"))
+        assert isdir(pjoin(input, "egamma"))
+        assert isdir(pjoin(input, "muons"))
+        res_mc = pjoin(options.results, "mc") if options.results else None
+        skm_mc = pjoin(options.skims, "mc") if options.skims else None
+        res_egamma = pjoin(options.results, "egamma") if options.results else None
+        skm_egamma = pjoin(options.skims, "egamma") if options.skims else None
+        res_muons = pjoin(options.results, "muons") if options.results else None
+        skm_muons = pjoin(options.skims, "muons") if options.skims else None
+        cmds.extend(get_a4_cmds(cmd + "_mc", glob(pjoin(input, "mc","*")), res_mc, skm_mc))
+        cmds.extend(get_a4_cmds(cmd + "_egamma", glob(pjoin(input, "egamma","*")), res_egamma, skm_egamma))
+        cmds.extend(get_a4_cmds(cmd + "_muons", glob(pjoin(input, "muons","*")), res_muons, skm_muons))
+    else:
+        cmds = get_a4_cmds(cmd, inputs, options.results, options.skims)
+    process(cmds, options.dryrun)
 
