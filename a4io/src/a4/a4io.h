5130a1629e4f3533574b2c49e2356453134cb2af
Johannes Ebke <ebke@cern.ch>
2011-09-10 04:40:43 +0200

    added prototypes to a4io.h

--- a4io/src/a4/a4io.h
+++ a4io/src/a4/a4io.h
@@ -1,5 +1,12 @@
 #ifndef HAVE_A4IO
 #define HAVE_A4IO
 
+namespace a4{
+    namespace io{
+        class A4InputStream;
+        class A4OutputStream;
+        template <typename ProtoClass> class UseClassID;
+    }
+}
 
 #endif
