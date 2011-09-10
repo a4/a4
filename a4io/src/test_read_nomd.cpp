bb2d2778a85747b3b9a25374b02c63e66406987c
Johannes Ebke <ebke@cern.ch>
2011-09-10 04:41:12 +0200

    added read test for no metadata

---
+++ a4io/src/test_read_nomd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "a4/input_stream.h"
+#include "a4/proto/io/A4Stream.pb.h"
+
+using namespace std;
+using namespace a4::io;
+
+int main(int argc, char ** argv) {
+    std::string fn;
+    if (argc == 1) {
+        fn = "test.a4";
+    } else if (argc == 2) {
+        fn = argv[1];
+    } else assert(argc <= 2);
+
+    A4InputStream r(fn);
+
+    int cnt = 0;
+    while (r.is_good()) {
+        ReadResult rr = r.next();
+        if (rr.class_id == TestEvent::kCLASSIDFieldNumber) {
+            auto te = static_shared_cast<TestEvent>(rr.object);
+            //std::cout << "TestEvent: " << te->event_number() << std::endl;
+            cnt++;
+        } else if (rr == READ_ERROR) throw "up";
+    }
+    return 0;
+}
