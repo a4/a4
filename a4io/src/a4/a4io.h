2abc3c483d6009b1d7c22c4a160cd5bae77182ea
Johannes Ebke <ebke@cern.ch>
2011-09-10 19:10:43 +0200

    added doxygen docs

--- a4io/src/a4/a4io.h
+++ a4io/src/a4/a4io.h
@@ -1,3 +1,22 @@
+/// A4IO
+/// ----
+///   The A4IO package provides A4 Streams - reading and writing
+///
+///   Concepts:
+///       * Event Stream: list of <Event> objects, with <MetaData> objects following after groups of events
+///       * Results: list of pairs (<Key>, <Result>), with <MetaData> following
+///
+///    <Event> objects can have arbitrary format
+///    <Result> objects _must_ be addable (operator + / __add__, can be a logical merge)
+///    <MetaData> objects must be addable and can define a difference measure, which makes it
+///       possibe to merge metadata depending on different factors,
+///       e.g per lumiblock, per run or per stream
+///
+///
+///    Streams of Events should be saved in .a4 files.
+///    Results are saved in .results files (which are also a4 streams).
+///
+
 #ifndef HAVE_A4IO
 #define HAVE_A4IO
 
---
+++ doc/doxygen/mainpage.h
@@ -0,0 +1,40 @@
+/*! \mainpage A4
+
+  Inspired and building on Samvel Khalatyan's excellent demonstration "RTvsPBThreads"
+   - http://indico.cern.ch/contributionDisplay.py?contribId=29&confId=141309
+   - https://github.com/ksamdev/RTvsPBThreads
+
+  A4 aims to provide a set of loosely coupled, fast libraries for HEP data analysis.
+  The current A4 libraries are:
+   - a4io: Input/Output Streams
+   - a4process: Tools to easily process events to histograms
+ 
+  Concepts (not yet finalized):
+   - Event Stream: list of <Event> objects, with <MetaData> objects following after groups of events
+   - Results: list of pairs (<Key>, <Result>), with <MetaData> following
+   - Processing: Analyzing events and producing Results and/or another Stream of Events
+
+  
+   - <Event> objects can have arbitrary format
+   - <Result> objects _must_ be addable (operator + / __add__, can be a logical merge)
+   - <MetaData> objects must be addable and can define a difference measure, which makes it
+    possibe to merge metadata depending on different factors,
+    e.g per lumiblock, per run or per stream
+
+   Streams of Events are saved in .a4 files.
+   Streams of Results are saved in .results files (which are also in the "a4" format).
+ 
+   Processing is done in executable files with the following syntax:
+
+   \code
+   ./my_analysis --config config_hww.ini --results my.results --output skim.a4 --input events.a4
+   \endcode
+
+   To setup an analysis, one can use
+  \code
+   a4_init_analysis
+  \endcode
+   in an empty directory. An A4 Analysis project will be generated
+*/
+
+
