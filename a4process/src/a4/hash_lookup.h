22f393c1c55d7f63712eb1dabeaac863fa87f80d
Johannes Ebke <ebke@cern.ch>
2011-09-15 19:44:13 +0200

    objectstore split from hash_lookup

---
+++ a4process/src/a4/hash_lookup.h
@@ -0,0 +1,58 @@
+#ifndef _A4_HASH_LOOKUP_H_
+#define _A4_HASH_LOOKUP_H_
+
+#include <vector>
+#include <map>
+#include <string>
+#include <iostream>
+
+/// \internal
+/// hash_lookup accelerates lookup of concatenated strings and numbers
+/// as occur often in analysis. It consists of a sequence of hash tables
+/// where each string and number is looked up.
+/// The implemtation uses "variadic templates", a very useful C++11 feature
+/// On insertion, it asserts that the const char * used is in a read-only
+/// section of memory to make sure no accidental overwriting takes place.
+class hash_lookup {
+    public:
+        /// Create an empty hash table
+        hash_lookup();
+
+        /// Lookup a piece of _data_ by const char * index
+        void * & lookup(const char * index);
+        /// Lookup a piece of _data_ by uint32_t index
+        void * & lookup(uint32_t index);
+        /// Lookup a _subhash_ by const char * index
+        template <typename... Args>
+        void * & lookup(const char * index, const Args& ...args);
+        /// Lookup a _subhash_ uint32_t index
+        template <typename... Args>
+        void * & lookup(uint32_t index, const Args& ...args);
+
+        /// Lookup a _subhash_ by const char * index
+        template <typename... Args>
+        hash_lookup * subhash(const char * index, const Args& ...args);
+        /// Lookup a _subhash_ uint32_t index
+        template <typename... Args>
+        hash_lookup * subhash(uint32_t index, const Args& ...args);
+
+    private:
+        const static uintptr_t size = 1<<16;
+        typedef struct { const char * cc_key; uint32_t ui_key; void * value; } hash_lookup_data;
+        const char * cc_key;
+        uint32_t ui_key;
+        hash_lookup_data _data[size];
+        hash_lookup * _subhash[size];
+};
+
+bool is_writeable_pointer(const char * _p);
+
+template<typename T, typename... Args>
+std::string str_printf(const char * s, const T& value, const Args&... args);
+
+template<typename T, typename... Args>
+std::string str_cat(const T& s, const Args&... args);
+
+#include "a4/hash_lookup_impl.h"
+
+#endif
