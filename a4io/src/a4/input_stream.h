5bd757a480d710c3f3a75f087498eb83534f2cc2
Johannes Ebke <ebke@cern.ch>
2011-09-18 21:22:39 +0200

    factored out A4Message into own header and as class

--- a4io/src/a4/input_stream.h
+++ a4io/src/a4/input_stream.h
@@ -6,7 +6,7 @@
 #include <deque>
 
 #include <a4/stream.h>
-
+#include <a4/message.h>
 
 // used internally
 namespace google{ namespace protobuf{ namespace io{
@@ -22,39 +22,6 @@ namespace a4{ namespace io{
     class GzipInputStream;
     class A4StartCompressedSection;
     class A4EndCompressedSection;
-
-    /// Wrapped message returned from the InputStream
-    typedef struct A4Message {
-        /// Construct A4Message that signifies end of stream or stream error
-        A4Message(bool error=false) { if (error) class_id = 1; else class_id = 0; message.reset(); };
-        /// Construct normal A4Message with class_id and protobuf Message
-        A4Message(uint32_t cls, shared<Message> msg) : class_id(cls), message(msg) {};
-        /// Class ID of the message read
-        uint32_t class_id;
-        /// shared protobuf message 
-        shared<Message> message;
-
-        /// true if an error occurred
-        bool error() const {return class_id == 1; };
-        /// true if the stream has terminated correctly
-        bool end() const {return class_id == 0; };
-        /// true if the message pointer is None (end, unknown or no metadata)
-        bool null() const {return message.get() == NULL; };
-        /// this object can be used in if() expressions, it will be true if it contains a message
-        operator bool() { return !null(); }
-        /// Check if the class ID matches.
-        /// example: if (result.is<TestEvent>())
-        template <class T>
-        bool is() const { return T::kCLASSIDFieldNumber == class_id; };
-        /// Check if the class ID matches and return the message, otherwise NULL.
-        /// example: auto event = result.as<MyEvent>()
-        template <class T>
-        shared<T> as() const {
-            if (not is<T>()) return shared<T>(); 
-            else return static_shared_cast<T>(message);
-        }
-    } A4Message;
-
 
     /// A4 Input Stream - reads protobuf messages from file
 
---
+++ a4io/src/a4/message.h
@@ -0,0 +1,50 @@
+#ifndef _A4_MESSAGE_H_
+#define _A4_MESSAGE_H_
+
+#include <string>
+#include <vector>
+#include <deque>
+
+#include <a4/a4io.h>
+
+// used internally
+namespace google{ namespace protobuf{
+    class Message;
+};};
+
+namespace a4{ namespace io{
+    /// Wrapped message returned from the InputStream
+    class A4Message {
+        public:
+            /// Construct A4Message that signifies end of stream or stream error
+            A4Message(bool error=false) { if (error) class_id = 1; else class_id = 0; message.reset(); };
+            /// Construct normal A4Message with class_id and protobuf Message
+            A4Message(uint32_t cls, shared<Message> msg) : class_id(cls), message(msg) {};
+            /// Class ID of the message read
+            uint32_t class_id;
+            /// shared protobuf message 
+            shared<Message> message;
+
+            /// true if an error occurred
+            bool error() const {return class_id == 1; };
+            /// true if the stream has terminated correctly
+            bool end() const {return class_id == 0; };
+            /// true if the message pointer is None (end, unknown or no metadata)
+            bool null() const {return message.get() == NULL; };
+            /// this object can be used in if() expressions, it will be true if it contains a message
+            operator bool() { return !null(); }
+            /// Check if the class ID matches.
+            /// example: if (result.is<TestEvent>())
+            template <class T>
+            bool is() const { return T::kCLASSIDFieldNumber == class_id; };
+            /// Check if the class ID matches and return the message, otherwise NULL.
+            /// example: auto event = result.as<MyEvent>()
+            template <class T>
+            shared<T> as() const {
+                if (not is<T>()) return shared<T>(); 
+                else return static_shared_cast<T>(message);
+            }
+    };
+};};
+
+#endif
