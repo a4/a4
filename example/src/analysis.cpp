a80074583ed716aa89cfd1c546d788768a9776e8
Johannes Ebke <johannes@ebke.org>
2011-09-28 18:17:20 +0200

    Reworked example to add analysis, link with -la4, and use rpath instead of LD_LIBRARY_PATH

---
+++ example/src/analysis.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include <a4/application.h>
+
+#include "a4/proto/mya4/Event.pb.h"
+
+using namespace std;
+using namespace a4::process;
+
+class MyProcessor : public ProcessorOf<Event, EventStreamInfo> {
+  public:
+    virtual bool process(const Event & event) {
+        cout << event.event_number() << " MD: " << metadata().total_events() << endl;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return a4_main_process<MyProcessor>(argc, argv);
+};
--- example/src/rwtest.cpp
+++ example/src/rwtest.cpp
@@ -1,28 +1,32 @@
 #include <iostream>
 
-#include "a4/proto/io/A4Stream.pb.h"
-#include "a4/output.h"
-#include "a4/input.h"
+#include <a4/output.h>
+#include <a4/input.h>
+
+#include "a4/proto/mya4/Event.pb.h"
 
 using namespace std;
 using namespace a4::io;
 
+A4RegisterClass(Event);
+A4RegisterClass(EventStreamInfo);
+
 int main(int argc, char ** argv) {
     const int N = 1000;
     {
-        A4Output a4o("test_io.a4", "TestEvent");
+        A4Output a4o("test_io.a4", "Event");
 
         auto stream = a4o.get_stream();
-        stream->content_cls<TestEvent>();
-        stream->metadata_cls<TestMetaData>();
+        stream->content_cls<Event>();
+        stream->metadata_cls<EventStreamInfo>();
 
-        TestEvent e;
+        Event e;
         for(int i = 0; i < N; i++) {
             e.set_event_number(i);
             stream->write(e);
         }
-        TestMetaData m;
-        m.set_meta_data(N);
+        EventStreamInfo m;
+        m.set_total_events(N);
         stream->metadata(m);
     }
     {
@@ -31,10 +35,10 @@ int main(int argc, char ** argv) {
         while (shared<A4InputStream> stream = in.get_stream()) {
             int cnt = 0;
             while (A4Message msg = stream->next()) {
-                if (auto te = msg.as<TestEvent>()) {
-                    auto me = stream->current_metadata().as<TestMetaData>();
+                if (auto te = msg.as<Event>()) {
+                    auto me = stream->current_metadata().as<EventStreamInfo>();
                     assert(cnt++ == te->event_number());
-                    assert(me->meta_data() == N);
+                    assert(me->total_events() == N);
                 }
             }
             if (stream->error()) throw "AJS";
