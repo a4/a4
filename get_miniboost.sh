8c17c487ebe4feaa969744a1433081ea03f6a415
Johannes Ebke <johannes@ebke.org>
2011-09-30 11:34:36 +0200

    Make get_protobuf/miniboost also compile their respective packages

--- get_miniboost.sh
+++ get_miniboost.sh
@@ -5,6 +5,10 @@ miniboost_name=miniboost-1
 miniboost_pack=$miniboost_name.tar.bz2
 miniboost_url=http://www.ebke.org/$miniboost_pack
 
+echo "-------------------------------------------------"
+echo "A4: Acquiring source of needed Boost Libraries..."
+echo "-------------------------------------------------"
+
 if ! test -d "miniboost"; then
   if ! test -e $miniboost_pack; then
     echo "Downloading A4 miniboost library $miniboost_name..."
@@ -22,9 +26,31 @@ if ! test -d "miniboost"; then
     exit 1
   fi
   echo "Builtin Miniboost Library Setup complete!"
-  echo "It will be compiled & installed together with A4."
 else
   echo "Existing ./miniboost directory found!"
-  echo "It will be compiled & installed together with A4."
 fi
 
+pushd miniboost
+
+echo "--------------------------"
+echo "A4: Bootstrapping Boost..."
+echo "--------------------------"
+
+if ! ./bootstrap.sh --prefix=$PWD; then
+  echo "Boost bootstrap.sh failed! :("
+  exit 1
+fi
+
+echo "---------------------------------------"
+echo "A4: Compiling needed Boost Libraries..."
+echo "---------------------------------------"
+
+if ! ./b2 -j2 --prefix=$PWD; then
+  echo "Boost compilation failed! :("
+  exit 1
+fi
+popd
+
+echo "------------------------------------"
+echo "A4: Builtin Miniboost ready for use!"
+echo "------------------------------------"
--- get_protobuf.sh
+++ get_protobuf.sh
@@ -5,9 +5,13 @@ protobuf_name=protobuf-2.4.1
 protobuf_pack=$protobuf_name.tar.bz2
 protobuf_url=http://protobuf.googlecode.com/files/$protobuf_pack
 
+echo "-------------------------------------------------"
+echo "A4: Acquiring source of needed Boost Libraries..."
+echo "-------------------------------------------------"
+
 if ! test -d "protobuf"; then
   if ! test -e $protobuf_pack; then
-    echo "Downloading google protobuf library $protobuf_name..."
+    echo "Downloading A4 protobuf library $protobuf_name..."
     curl -f $protobuf_url > $protobuf_pack
     if test $? != 0; then
       echo "FATAL: Download failed! :( "
@@ -23,9 +27,32 @@ if ! test -d "protobuf"; then
   fi
   mv $protobuf_name protobuf
   echo "Builtin Protobuf Library Setup complete!"
-  echo "It will be compiled & installed together with A4."
 else
   echo "Existing ./protobuf directory found!"
-  echo "It will be compiled & installed together with A4."
 fi
 
+pushd protobuf
+
+echo "---------------------------"
+echo "A4: Configuring Protobuf..."
+echo "---------------------------"
+
+if ! ./configure --prefix=$PWD; then
+  echo "Protobuf configure failed! :("
+  echo "Edit ./get_protobuf.sh if you need to add additional options to ./configure"
+  exit 1
+fi
+
+echo "---------------------------------"
+echo "A4: Compiling Protobuf Library..."
+echo "---------------------------------"
+
+if ! make -j4 && make install; then
+  echo "Protobuf compilation failed! :("
+  exit 1
+fi
+popd
+
+echo "------------------------------------"
+echo "A4: Builtin Protobuf ready for use!"
+echo "------------------------------------"
