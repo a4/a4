4099f79c03d408ed3fba25f452c586886724edf3
Peter Waller <peter.waller@cern.ch>
2011-10-03 18:20:42 +0100

    Add the simplest D3PD dumper possible

---
+++ a4root/src/analysis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <a4/application.h>
+#include "Event.pb.h"
+
+using namespace std;
+using namespace a4::process;
+
+class MyProcessor : public ProcessorOf<Event, Metadata> {
+  public:
+    virtual bool process(const Event & event) {
+        cout << event.run_number() << " MD: " << metadata().total_events() << endl;
+    }
+};
+
+int main(int argc, const char * argv[]) {
+    return a4_main_process<MyProcessor>(argc, argv);
+};
