2356c4c788a7aa02f5389f3abcd90c20a005697a
Johannes Ebke <ebke@cern.ch>
2011-10-03 19:02:01 +0200

    Add A4_PROTOPYTHON_PATH to include more proto file definitions, default ./python

--- a4io/python/a4/proto/__init__.py
+++ a4io/python/a4/proto/__init__.py
@@ -2,8 +2,9 @@
 class_ids = {}
 
 def init():
-    from os import listdir
+    from os import listdir, environ
     from os.path import abspath, isdir, dirname, join, exists
+    import sys
     fd = abspath(dirname(__file__))
     for dir in listdir(fd):
         if isdir(join(fd, dir)):
@@ -12,6 +13,21 @@ def init():
             classes = (getattr(mod, k) for k in mod.__dict__.keys() if not k.startswith("_"))
             clsd = dict((c.CLASS_ID_FIELD_NUMBER, c) for c in classes if hasattr(c, "CLASS_ID_FIELD_NUMBER"))
             class_ids.update(clsd)
+
+    ppp = [abspath(p) for p in environ.get("A4_PROTOPYTHON_PATH","./python").split(":") if p and exists(p)]
+    uppp = set()
+    for p in ppp:
+        if p in uppp:
+            continue
+        uppp.add(p)
+        for dir in listdir(p):
+            if isdir(join(p, dir)):
+                sys.path.insert(0,p)
+                dir = dir.strip()
+                mod = __import__(dir, globals(), locals(), ["*"])
+                classes = (getattr(mod, k) for k in mod.__dict__.keys() if not k.startswith("_"))
+                clsd = dict((c.CLASS_ID_FIELD_NUMBER, c) for c in classes if hasattr(c, "CLASS_ID_FIELD_NUMBER"))
+                class_ids.update(clsd)
 
 init()
 del init
