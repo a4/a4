06fbd41b197f06fde1f7a1a03b28df5eb76a8334
Johannes Ebke <johannes@ebke.org>
2011-10-06 19:15:38 +0200

    Add test_histo to generate a test file for test_app

---
+++ a4process/src/test_histo.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+#include "a4/output_stream.h"
+#include "a4/input_stream.h"
+#include "A4Key.pb.h"
+
+using namespace std;
+using namespace a4::io;
+using namespace a4::process;
+
+int main(int argc, char ** argv) {
+    {
+        A4OutputStream w("test_histos.a4", "TestHisto");
+        w.content_cls<TestHisto>().metadata_cls<TestHistoMetaData>();
+
+        const int N = 1000;
+        TestHisto e;
+        for(int i = 0; i < N; i++) {
+            e.set_bin_number(i);
+            w.write(e);
+        }
+        TestHistoMetaData m;
+        m.set_meta_data(N);
+        w.write(m);
+
+    }
+    {
+        A4InputStream r("test_histos.a4");
+        int cnt = 0;
+        while (A4Message msg = r.next()) {
+            if (shared<TestHisto> te = msg.as<TestHisto>()) {
+                assert(cnt++ == te->bin_number());
+            }
+        }
+        if (r.error()) throw "AJS";
+    }
+    return 0;
+}
