8da19b6958371b5f195b80cacacc409a7fc21c79
Johannes Ebke <johannes@ebke.org>
2011-10-06 16:14:21 +0200

    Make autogen.sh a bit prettier

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 if autoreconf -i --force; then
     rm -rf autom4te.cache/
+    echo "------------------------------------------------------------"
+    echo "A4: AutoMake Setup completed."
     echo "A4: Disregard messages about LT_INIT and AC_CONFIG_MACRO_DIR" 
     echo "A4: These macros are in included files and are therefore not picked up." 
-    echo "A4: AutoMake Setup completed."
+    echo "------------------------------------------------------------"
+
 else
     echo "A4: autogen.sh failed."
 fi
