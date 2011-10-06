c2c5be1fd6b96c878adae2ecc6bdd82b764d8566
Johannes Ebke <ebke@cern.ch>
2011-10-06 19:26:37 +0200

    Miniboost generation script now includes filesystem library

--- common/make_miniboost.sh
+++ common/make_miniboost.sh
@@ -6,7 +6,7 @@ if test -z "$BOOST_ROOT"; then
   exit 1
 fi
 rm $mboost -rf && mkdir $mboost
-bcp --boost=$BOOST_ROOT program_options foreach thread $mboost
+bcp --boost=$BOOST_ROOT program_options foreach thread filesystem $mboost
 cp -r $BOOST_ROOT/boost-build.jam  $BOOST_ROOT/boostcpp.jam $BOOST_ROOT/bootstrap.sh $mboost/
 mkdir -p $mboost/tools/build
 cp -r $BOOST_ROOT/tools/build/v2 $mboost/tools/build
