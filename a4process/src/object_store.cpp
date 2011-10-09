7364e4f33599d52b41ccfeee23ba076dbb0c0b47
Johannes Ebke <johannes@ebke.org>
2011-10-09 02:18:58 +0200

    Make ObjectStore live on the heap rather than on the stack

--- a4process/src/object_store.cpp
+++ a4process/src/object_store.cpp
@@ -12,15 +12,18 @@ A4RegisterClass(a4::process::TestHistoMetaData);
 
 namespace a4{ namespace process{
 
-    ObjectBackStore::ObjectBackStore() {};
+    ObjectBackStore::ObjectBackStore() {
+        hl.reset(new hash_lookup());
+        _store.reset(new std::map<std::string, shared<Storable>>());
+    };
 
     ObjectBackStore::~ObjectBackStore() {};
 
-    ObjectStore ObjectBackStore::store() { return ObjectStore(&hl, this); };
+    ObjectStore ObjectBackStore::store() { return ObjectStore(hl.get(), this); };
 
     void ObjectBackStore::to_stream(a4::io::A4OutputStream &outs) const {
         std::map<std::string, shared<Storable>>::const_iterator i;
-        for(i = _store.begin(); i != _store.end(); i++) {
+        for(i = _store->begin(); i != _store->end(); i++) {
             A4Key k;
             k.set_name(i->first);
             shared<const google::protobuf::Message> msg(i->second->as_message());
@@ -37,7 +40,7 @@ namespace a4{ namespace process{
             if (!key.is<A4Key>()) continue;
             a4::io::A4Message val = ins.next();
             std::string name = key.as<A4Key>()->name();
-            _store[name] = message_to_storable(val);
+            (*_store)[name] = message_to_storable(val);
         }
     
     };
@@ -45,7 +48,7 @@ namespace a4{ namespace process{
     std::vector<std::string> ObjectBackStore::list() const {
         std::vector<std::string> res;
         std::map<std::string, shared<Storable>>::const_iterator i;
-        for (i = _store.begin(); i != _store.end(); i++) {
+        for (i = _store->begin(); i != _store->end(); i++) {
             res.push_back(i->first);
         }
         return res;
