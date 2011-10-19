a6035976cfa017cd34440a39da8970db74da4f2d
Peter Waller <peter.waller@cern.ch>
2011-10-19 12:23:27 +0100

    common/miniboost: Automatic discovery of used boost packages

--- common/discover_used_boost.sh
+++ common/discover_used_boost.sh
@@ -1,5 +1,20 @@
 #! /usr/bin/env bash
 
+set -u -e
+
+# Why: This generates a list of used boost headers so that we can make a 
+#      stripped down library containing just what we use.
+
+# HOW TO:
+# 1) Run this script
+# 2) Commit the updated `common/used_boost_headers`
+# 3) Run `common/make_miniboost.sh`
+
+hash ack 2>&- || { echo >&2 "I use \`ack\` to discover files, but it isn't available. Aborting."; exit 1; }
+
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-ack --no-heading '#include <boost/' ${SCRIPT_DIR}/../a4* | cut -d: -f3- | sort -u | sed -r 's|^#include <boost/(.*).hpp>|\1|' | xargs
+ack --no-heading '#include <boost/' ${SCRIPT_DIR}/../a4* | 
+    cut -d: -f3- | 
+    sort -u | 
+    sed -r 's|^#include <boost/(.*).hpp>|\1|' > ${SCRIPT_DIR}/used_boost_headers
--- common/make_miniboost.sh
+++ common/make_miniboost.sh
@@ -1,12 +1,24 @@
-#!/bin/sh
+#! /bin/sh
+
+set -u -e
+
+# HOWTO:
+# 1) Install boost/bcp
+#  1a) If bcp isn't installed, build it and run "./b2 tools/bcp install"
+# 2) Check that "common/used_boost_headers" is uptodate by running "common/discover_used_boost"
+# 3) Make sure bcp is in your path and set BOOST_ROOT to boost's installation include directory
+#  3a) or do "BOOST_ROOT=. PATH=dist/bin:$PATH ~/Projects/a4/common/make_miniboost.sh"
+#      from boost's installation directory 
+
 mboost=miniboost-1.47
 if test -z "$BOOST_ROOT"; then
   echo "Set BOOST_ROOT to your Boost directory!"
-  echo "If you use your system Boost, export BOOST_ROOT=/usr/include"
   exit 1
 fi
 rm $mboost -rf && mkdir $mboost
-bcp --boost=$BOOST_ROOT program_options foreach thread filesystem $mboost
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+USED_PACKAGES=$(cat ${SCRIPT_DIR}/used_boost_headers | xargs)
+bcp --boost=$BOOST_ROOT $USED_PACKAGES $mboost
 cp -r $BOOST_ROOT/boost-build.jam  $BOOST_ROOT/boostcpp.jam $BOOST_ROOT/bootstrap.sh $mboost/
 mkdir -p $mboost/tools/build
 cp -r $BOOST_ROOT/tools/build/v2 $mboost/tools/build
