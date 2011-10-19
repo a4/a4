f13d6156e8c4728f867a670b012cdd9d9e8e0c33
Johannes Ebke <johannes@ebke.org>
2011-10-19 13:50:20 +0200

    common: Replace ack with grep in discover_used_boost.sh

--- common/discover_used_boost.sh
+++ common/discover_used_boost.sh
@@ -10,11 +10,7 @@ set -u -e
 # 2) Commit the updated `common/used_boost_headers`
 # 3) Run `common/make_miniboost.sh`
 
-hash ack 2>&- || { echo >&2 "I use \`ack\` to discover files, but it isn't available. Aborting."; exit 1; }
-
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-ack --no-heading '#include <boost/' ${SCRIPT_DIR}/../a4* | 
-    cut -d: -f3- | 
-    sort -u | 
+grep -sh '#include <boost/' $(find ${SCRIPT_DIR}/../a4* -name '*.cpp' -or -name '*.h') | 
+    sort -u |
     sed -r 's|^#include <boost/(.*).hpp>|\1|' > ${SCRIPT_DIR}/used_boost_headers
