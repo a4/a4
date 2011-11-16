d85c3f96cf7433c2dfa7319edbf79d9fcd082067
Johannes Ebke <johannes@ebke.org>
2011-11-16 12:26:59 +0100

    a4io: Improved exception handling with GDB

--- a4io/src/a4/exceptions.h
+++ a4io/src/a4/exceptions.h
@@ -1,14 +1,10 @@
 #ifndef _A4_EXCEPTIONS_
 #define _A4_EXCEPTIONS_
 
-#include <execinfo.h>
-#include <signal.h>
-
 #include <exception>
 #include <iostream>
 #include <fstream>
 #include <sstream>
-#include <cxxabi.h>
 #include <cstdlib>
 
 #include <boost/filesystem.hpp>
@@ -19,83 +15,13 @@ namespace a4{
 
     class BackTraceException : public std::exception {
         public:
-            BackTraceException() {
-
-                // Try using GDB first...
-                bool gdb_worked = true;
-                FILE *fpipe;
-                std::stringstream cmd1, cmd2, res1, res2;
-                cmd1 << "gdb -n --batch --eval-command='set complaints 0' --eval-command='attach " << getpid() << "' --eval-command='thread apply all bt'";
-                cmd2 << "gdb -n --batch --eval-command='set complaints 0' --eval-command='attach " << getpid() << "' --eval-command='thread apply all bt full'";
-                char line[256];
-                if ( !(fpipe = (FILE*)popen(cmd1.str().c_str(),"r")) ) gdb_worked = false;
-                else while (fgets( line, sizeof line, fpipe)) res1 << line;
-                if (pclose(fpipe) != 0) gdb_worked = false;
-                if ( !(fpipe = (FILE*)popen(cmd2.str().c_str(),"r")) ) gdb_worked = false;
-                else while (fgets( line, sizeof line, fpipe)) res2 << line;
-                if (pclose(fpipe) != 0) gdb_worked = false;
-                _backtrace = res1.str();
-                _full_backtrace = res2.str();
-                if (gdb_worked) return;
-
-                // If GDB did not work...
-                void * stack_funcs[50];
-                int stack_size = backtrace(stack_funcs, 50);
-                char ** symbols = backtrace_symbols(stack_funcs, stack_size);
-                std::stringstream sstr;
-
-                // skip first stack frame (points here)
-                for (int i = 1; i < stack_size && symbols != NULL; i++) {
-
-                    // find parantheses and +address offset surrounding mangled name
-                    char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
-                    for (char *p = symbols[i]; *p; p++) {
-                        if (*p == '(') {
-                            mangled_name = p; 
-                        } else if (*p == '+') {
-                            offset_begin = p;
-                        } else if (*p == ')') {
-                            offset_end = p;
-                            break;
-                        }
-                    }
-
-                    // if the line could be processed, attempt to demangle the symbol
-                    if (mangled_name && offset_begin && offset_end && 
-                        mangled_name < offset_begin) {
-                        *mangled_name++ = '\0';
-                        *offset_begin++ = '\0';
-                        *offset_end++ = '\0';
-
-                        int status;
-                        char * real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
-
-                        // if demangling is successful, output the demangled function name
-                        if (status == 0) {    
-                            sstr << "[" << i << "] " << symbols[i] << " : " 
-                                      << real_name << "+" << offset_begin << offset_end 
-                                      << std::endl;
-                        } else {
-                        // otherwise, output the mangled function name
-                            sstr << "[" << i << "] " << symbols[i] << " : " 
-                                      << mangled_name << "+" << offset_begin << offset_end 
-                                      << std::endl;
-                        }
-                        free(real_name);
-                    } else {
-                    // otherwise, print the whole line
-                        sstr << "[" << i << "] " << symbols[i] << std::endl;
-                    }
-                }
-                free(symbols);
-                _backtrace = sstr.str();
-                _full_backtrace = sstr.str();
-            };
+            BackTraceException();
             virtual ~BackTraceException() throw() {};
             virtual const char* what() const throw() {
                 return _backtrace.c_str();
             }
         protected:
+            static volatile bool called;
             std::string _backtrace;
             std::string _full_backtrace;
     };
@@ -128,18 +54,14 @@ namespace a4{
                 sstr << "If this is a bug in A4, please submit an issue at https://github.com/JohannesEbke/a4/issues" << std::endl;
                 _what = sstr.str();
             }
+
             virtual ~Fatal() throw() {};
+
             virtual const char* what() const throw() {
                 return _what.c_str();
             }
 
-            static bool enable_throw_on_segfault() {
-                struct sigaction act;
-                act.sa_handler = &segfault_handler;
-                sigemptyset(&act.sa_mask);
-                act.sa_flags = 0;
-                return sigaction(SIGSEGV, &act, NULL) == 0;
-            };
+            static bool enable_throw_on_segfault();
 
             static void segfault_handler(int i) {
                 throw Fatal("Segmentation Fault!");
---
+++ a4io/src/exceptions.cpp
@@ -0,0 +1,110 @@
+#include <a4/exceptions.h>
+
+#include <execinfo.h>
+#include <signal.h>
+#include <cxxabi.h>
+
+namespace a4{
+
+    BackTraceException::BackTraceException() {
+        // Enter an infinite loop - only one fatal should be executing.
+        // This is not excessively important, so this sync should be enough.
+        if (called) { while(true); return; } else called = true;
+        //std::cerr << "A4: An error occurred - trying to attach gdb to the current process --" << std::endl;
+        // Try using GDB first...
+        bool gdb_worked = true;
+        FILE *fpipe;
+        std::stringstream cmd1, cmd2, res1, res2, res3;
+        cmd1 << "gdb -n --batch --eval-command='set complaints 0' --eval-command='attach " << getpid() << "' --eval-command='thread apply all bt' 2>&1";
+        cmd2 << "gdb -n --batch --eval-command='set complaints 0' --eval-command='attach " << getpid() << "' --eval-command='thread apply all bt full' 2>&1";
+        char line[256];
+        if ( !(fpipe = (FILE*)popen(cmd1.str().c_str(),"r")) ) gdb_worked = false;
+        else while (fgets( line, sizeof line, fpipe)) res1 << line;
+        if (pclose(fpipe) != 0) gdb_worked = false;
+        if ( !(fpipe = (FILE*)popen(cmd2.str().c_str(),"r")) ) gdb_worked = false;
+        else while (fgets( line, sizeof line, fpipe)) res2 << line;
+        if (pclose(fpipe) != 0) gdb_worked = false;
+
+        res1.seekg(0);
+        while(!res1.eof()) {
+            std::string ln;
+            std::getline(res1, ln);
+            if(ln.substr(0, 31) == "ptrace: Operation not permitted.") {
+                gdb_worked = false;
+                break;
+            }
+            if(ln.substr(0, 7) == "warning:") continue;
+            res3 << "GDB: " << ln << std::endl;
+        }
+
+        _backtrace = res3.str();
+        _full_backtrace = res2.str();
+        if (gdb_worked) return;
+        //std::cerr << "A4: GDB failed to attach to the current process. Trying to extract backtrace manually... " << std::endl;
+
+        // If GDB did not work...
+        void * stack_funcs[50];
+        int stack_size = backtrace(stack_funcs, 50);
+        char ** symbols = backtrace_symbols(stack_funcs, stack_size);
+        std::stringstream sstr;
+
+        // skip first stack frame (points here)
+        for (int i = 1; i < stack_size && symbols != NULL; i++) {
+
+            // find parantheses and +address offset surrounding mangled name
+            char *mangled_name = 0, *offset_begin = 0, *offset_end = 0;
+            for (char *p = symbols[i]; *p; p++) {
+                if (*p == '(') {
+                    mangled_name = p; 
+                } else if (*p == '+') {
+                    offset_begin = p;
+                } else if (*p == ')') {
+                    offset_end = p;
+                    break;
+                }
+            }
+
+            // if the line could be processed, attempt to demangle the symbol
+            if (mangled_name && offset_begin && offset_end && 
+                mangled_name < offset_begin) {
+                *mangled_name++ = '\0';
+                *offset_begin++ = '\0';
+                *offset_end++ = '\0';
+
+                int status;
+                char * real_name = abi::__cxa_demangle(mangled_name, 0, 0, &status);
+
+                // if demangling is successful, output the demangled function name
+                if (status == 0) {    
+                    sstr << "[" << i << "] " << symbols[i] << " : " 
+                              << real_name << "+" << offset_begin << offset_end 
+                              << std::endl;
+                } else {
+                // otherwise, output the mangled function name
+                    sstr << "[" << i << "] " << symbols[i] << " : " 
+                              << mangled_name << "+" << offset_begin << offset_end 
+                              << std::endl;
+                }
+                free(real_name);
+            } else {
+            // otherwise, print the whole line
+                sstr << "[" << i << "] " << symbols[i] << std::endl;
+            }
+        }
+        free(symbols);
+        _backtrace = sstr.str();
+        _full_backtrace = sstr.str();
+    };
+
+    bool Fatal::enable_throw_on_segfault() {
+        struct sigaction act;
+        act.sa_handler = &segfault_handler;
+        sigemptyset(&act.sa_mask);
+        act.sa_flags = 0;
+        return sigaction(SIGSEGV, &act, NULL) == 0;
+    };
+
+    volatile bool BackTraceException::called = false;
+
+};
+
