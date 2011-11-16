1807677078aba36160d3b468a4f2480cb6c7ee73
Johannes Ebke <johannes@ebke.org>
2011-11-16 11:35:29 +0100

    a4io: Add processing files output

--- a4io/src/input.cpp
+++ a4io/src/input.cpp
@@ -33,6 +33,7 @@ void A4Input::report_finished(A4Input * input, InputStream* _s) {
     if (_s->end()) {
         assert(input->_processing.erase(_s) == 1);
         input->_finished.insert(_s);
+        std::cerr << "Finished processing " << _s->str() << std::endl;
     } else if (_s->error() || input->_resched_count[_s] > 0) {
         std::cerr << "ERROR - a4::io::A4Input - '" << _s->str() << "' encountered an error during reading!" << std::endl;
         input->_error.insert(_s);
@@ -56,7 +57,7 @@ shared<InputStream> A4Input::get_stream() {
     //auto ret = shared<InputStream>(s, (new Callback(this))->Call);
     std::function<void (InputStream*)> cb = std::bind(&A4Input::report_finished, this, std::placeholders::_1);
     shared<InputStream> ret(s, cb);
-    //std::cerr << "Input requested, returning " << s->str() << std::endl;
+    std::cerr << "Starting to process " << s->str() << std::endl;
     return ret;
 }
 
