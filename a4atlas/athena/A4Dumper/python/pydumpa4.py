96154b897354eb988ec5c3794acb558521c79987
Johannes Ebke <johannes@ebke.org>
2012-01-12 01:57:59 +0100

    a4atlas: Activate JVF recalculation

--- a4atlas/athena/A4Dumper/python/pydumpa4.py
+++ a4atlas/athena/A4Dumper/python/pydumpa4.py
@@ -876,7 +876,7 @@ else:
     athena_setup(None, -1)
 
 # JVF fix by Scott Snyder
-if False:
+if True:
     from RecExConfig.RecFlags import rec
     rec.UserExecs = ["myjets()"]
     def myjets():
@@ -884,8 +884,9 @@ if False:
 
         from JetMomentTools.SetupJetMomentTools import getJetVertexAssociationTool
         jvatool = getJetVertexAssociationTool('AntiKt', 0.4, 'Topo') # parameters are irrelevant, these will work for any jets
-        make_JetMomentGetter( 'AntiKt4TopoJets' , [jvatool] ) 
+        #make_JetMomentGetter( 'AntiKt4TopoJets' , [jvatool] ) 
         make_JetMomentGetter( 'AntiKt4TopoEMJets' , [jvatool] ) 
+
 
 # do autoconfiguration of input
 include ("RecExCommon/RecExCommon_topOptions.py")
