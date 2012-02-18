09bbec11de2e3d53e970eb8b8884ff6a0e6f1282
Peter Waller <peter.waller@gmail.com>
2012-02-18 21:38:41 +0000

    Fix #64 as best we can. Doesn't seem to work exactly correctly on my modern CPU but not worth the time investment

--- a4process/src/a4/cpu_info.h
+++ a4process/src/a4/cpu_info.h
@@ -8,7 +8,7 @@
 
 struct cpuinfo_t {
     unsigned int physical_cores,
-                 logical_cores;
+                   logical_cores;
     bool hyperthreading;
     std::string vendor;
 };
@@ -19,7 +19,12 @@ void cpuid(unsigned i, unsigned regs[4]) {
 
 #else
   asm volatile
-    ("cpuid" : "=a" (regs[0]), "=b" (regs[1]), "=c" (regs[2]), "=d" (regs[3])
+    ("cpuid" :
+        // Weird ordering is intentional.
+        "=a" (regs[0]),
+        "=b" (regs[1]),
+        "=c" (regs[2]),
+        "=d" (regs[3])
      : "a" (i), "c" (0));
   // ECX is set to zero for cpuid function 4
 #endif
@@ -32,28 +37,36 @@ struct cpuinfo_t get_cpuinfo() {
     union {
         char vendor[12];
         unsigned regs[4];
+        struct {
+            volatile unsigned eax, ebx, ecx, edx;
+        } reg;
     };
+    
     cpuid(0, regs);
+    std::swap(reg.eax, reg.ebx);
+    std::swap(reg.ebx, reg.edx);
     cpuinfo.vendor = std::string(vendor, 12);
 
     // Get CPU features
     cpuid(1, regs);
-    unsigned cpuFeatures = regs[3]; // EDX
+    unsigned cpuFeatures = reg.edx; // EDX
 
     // Logical core count per CPU
     cpuid(1, regs);
-    cpuinfo.logical_cores = (regs[1] >> 16) & 0xff; // EBX[23:16]
+    cpuinfo.logical_cores = (reg.ebx >> 16) & 0xff; // EBX[23:16]
     cpuinfo.physical_cores = cpuinfo.logical_cores;
 
-    if (cpuinfo.vendor == "GenuineIntel") {
+    if (true && cpuinfo.vendor == "GenuineIntel") {
         // Get DCP cache info
         cpuid(4, regs);
-        cpuinfo.physical_cores = ((regs[0] >> 26) & 0x3f) + 1; // EAX[31:26] + 1
+        cpuinfo.physical_cores = ((reg.eax >> 26) & 0x3f) + 1; // EAX[31:26] + 1
     } else if (cpuinfo.vendor == "AuthenticAMD") {
         // Get NC: Number of CPU cores - 1
         cpuid(0x80000008, regs);
-        cpuinfo.physical_cores = ((unsigned)(regs[2] & 0xff)) + 1; // ECX[7:0] + 1
+        cpuinfo.physical_cores = ((unsigned)(reg.ecx & 0xff)) + 1; // ECX[7:0] + 1
     } else {
+        WARNING("Unknown processor type '", cpuinfo.vendor,
+                "' (Expected 'GenuineIntel' or 'AuthenticAMD')");
         cpuinfo.physical_cores = 1;
         cpuinfo.logical_cores = 1;
         cpuinfo.hyperthreading = 0;
@@ -63,5 +76,6 @@ struct cpuinfo_t get_cpuinfo() {
     // Detect hyper-threads  
     cpuinfo.hyperthreading = cpuFeatures & (1 << 28) && 
                              cpuinfo.physical_cores < cpuinfo.logical_cores;
+    
     return cpuinfo;
 }
