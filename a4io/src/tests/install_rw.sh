3a8df89ae64aa0318a55e42114acfcdd565530bb
Johannes Ebke <johannes@ebke.org>
2012-02-23 19:06:10 +0100

    waf: Fix final test

--- a4io/src/tests/install_rw.sh
+++ a4io/src/tests/install_rw.sh
@@ -38,12 +38,12 @@ rm test_fw1.a4 test_bw1.a4
 ../read test_fwbw.a4 
 ../read test_bwfw.a4 
 ../read test_bwbw.a4
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fw.a4",1000))'
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bw.a4",1000))'
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fwfw.a4",2000))'
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fwbw.a4",2000))'
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bwfw.a4",2000))'
-${PYTHON} -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bwbw.a4",2000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fw.a4",1000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bw.a4",1000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fwfw.a4",2000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_fwbw.a4",2000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bwfw.a4",2000))'
+python -c 'from a4.stream import test_read; import sys; sys.exit(test_read("test_bwbw.a4",2000))'
 rm -f test_fw.a4 test_bw.a4 test_fwfw.a4 test_bwfw.a4 test_fwbw.a4 test_bwbw.a4
 rm -f pytest_fw.a4 pytest_bw.a4 pytest_fwfw.a4 pytest_bwfw.a4 pytest_fwbw.a4 pytest_bwbw.a4
 rm -f test_rw.a4 test_mm.a4
