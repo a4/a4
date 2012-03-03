98b4b4ff4c604d800379c8c6fcbaf32f430f122a
Johannes Ebke <johannes@ebke.org>
2012-03-03 17:57:19 +0100

    waf: Update to 1.7

--- common/waf/libtool.py
+++ common/waf/libtool.py
@@ -125,7 +125,7 @@ def apply_libtool(self):
 			self.env.append_unique('LINKFLAGS', v)
 
 
-Task.task_type_from_func('fakelibtool', vars=fakelibtool_vardeps, func=fakelibtool_build, color='BLUE', after="link stlink")
+Task.task_factory('fakelibtool', vars=fakelibtool_vardeps, func=fakelibtool_build, color='BLUE', after="link stlink")
 
 class libtool_la_file:
 	def __init__ (self, la_filename):
