61927cbc5787c2a398edac070160ae8106bb1638
Peter Waller <p@pwaller.net>
2013-02-12 12:05:58 +0000

    a4copy: add --debug-salient to extract one event per metadata

--- a4process/src/apps/a4copy.cpp
+++ a4process/src/apps/a4copy.cpp
@@ -8,9 +8,18 @@
 class CopyProcessor : public a4::process::Processor {
 public:
     bool _metadata_only;
+    bool _salient; // --debug-salient was specified
+    bool _fresh_metadata; // encountered new metadata since last event
+    
     uint64_t _start_index;
     
-    CopyProcessor() : _metadata_only(false), _start_index(0) {}
+    CopyProcessor() : _metadata_only(false), _salient(false),
+        _fresh_metadata(true), _start_index(0) {}
+    
+    virtual shared<a4::io::A4Message> process_new_metadata() {
+        _fresh_metadata = true;
+        return shared<a4::io::A4Message>();
+    }
 
     virtual void process_message(shared<const a4::io::A4Message> m) {
         if (_metadata_only) {
@@ -23,16 +32,27 @@ public:
         static size_t i = 0;
         if (i++ < _start_index)
             return;
+        
         if (i % 1000 == 0)
             INFO("Copied ", i, " events..");
-        write(m);
+
+        // Nested to make logic blindingly clear
+        if (_salient) {
+            if (_fresh_metadata) {
+                write(m);
+            }
+        } else {
+            write(m);
+        }
+        
+        _fresh_metadata = false;
     }
 };
 
 class CopyConfiguration : public a4::process::ConfigurationOf<CopyProcessor> {
 public:
 
-    bool _metadata_only;
+    bool _metadata_only, _salient;
     uint64_t _start_index;
 
     CopyConfiguration() : _metadata_only(false), _start_index(0) {}
@@ -40,6 +60,7 @@ public:
     void add_options(po::options_description_easy_init opt) {
         opt("metadata-only,M", po::bool_switch(&_metadata_only), "Only copy metadata");
         opt("start-index", po::value(&_start_index), "Start copy from a particular index");
+        opt("debug-salient", po::bool_switch(&_salient), "Copy one event per metadata");
     }
 
     void setup_processor(CopyProcessor& p) {
@@ -50,6 +71,7 @@ public:
             p._start_index = _start_index;
         }
         p._metadata_only = _metadata_only;
+        p._salient = _salient;
     }
 };
 
